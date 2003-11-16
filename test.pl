b0f8db6da887fbe778dd53bea4de6cb16aeede0d
David Cantrell <david@cantrell.org.uk>
2003-11-16 15:10:21 +0000

    Import of DCANTRELL/Statistics-ChiSquare-0.5 from CPAN.
    gitpan-cpan-distribution: Statistics-ChiSquare
    gitpan-cpan-version:      0.5
    gitpan-cpan-path:         DCANTRELL/Statistics-ChiSquare-0.5.tar.gz
    gitpan-cpan-author:       DCANTRELL
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -1,22 +1,11 @@
-# Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl test.pl'
-
-######################### We start with some black magic to print on failure.
-
-# change 'tests => 1' to 'tests => last_test_to_print';
-
 use Test;
-BEGIN { plan tests => 2 };
+BEGIN { plan tests => 4 };
 use Statistics::ChiSquare;
-@coin = (500, 500);
-ok(1); # If we made it this far, we're ok.
+ok(1);
 
-######################### End of black magic.
-
-# Insert your test code below (better if it prints "ok 13"
-# (correspondingly "not ok 13") depending on the success of chunk 13
-# of the test code):
-
-ok(Statistics::ChiSquare::chisquare(@coin),
+ok(Statistics::ChiSquare::chisquare(500,500),
    "There's a >99% chance, and a <100% chance, that this data is random.");
-
+ok(Statistics::ChiSquare::chisquare(map { 500 } 1..28),
+   "There's a >99% chance, and a <100% chance, that this data is random.");
+ok(Statistics::ChiSquare::chisquare(1..28),
+   "There's a <1% chance that this data is random.");
